fe2d7d074d73a0fa44754ad7a600cc5a8c6ad6ec
Lightening Gibson <lighteningibson@gmail.com>
2022-01-11 15:01:17 -0600

    Completeed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    name: '',
+    streetAddress: '' 
+}
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value){
+   
+    const employeeCopy = {...employee}
+    employeeCopy[streetAddress] = value;
+    
+    return employeeCopy;
+}
+//employee.name = 'Sam'
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee[key] = value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+    const employeeCopy2 = {...employee}
+    delete employeeCopy2[key];
+    
+    return employeeCopy2;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key];
+
+    return employee;
+}
